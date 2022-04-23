0d9bffffac0b53bda0871065e020d9c38a1b8a81
Raven <krzysztof.ekiert@gmail.com>
2022-04-23 16:02:41 +0200

    'test2'

--- unittests_cesar.py
+++ unittests_cesar.py
@@ -5,8 +5,16 @@ from cesar import decrypt
 
 class TestEncrypt(unittest.TestCase):
     def test_simple_encrypt(self):
-        self.assertEqual(encrypt("A", 1), "C")
+        self.assertEqual(encrypt("A", 1), "B")
+
+class TestEncrypt2(unittest.TestCase):
+    def test_simple_encrypt(self):
+        self.assertEqual(encrypt("CEASER CIPHER DEMO", 4), "GIEWIV GMTLIV HIQS")
 
 class TestDecrypt(unittest.TestCase):
     def test_simple_decrypt(self):
-        self.assertEqual(encrypt("C", 1), "A")
+        self.assertEqual(encrypt("B", 1), "A")
+
+class TestDecrypt2(unittest.TestCase):
+    def test_simple_decrypt(self):
+        self.assertEqual(encrypt("GIEWIV GMTLIV HIQS", 4), "CEASER CIPHER DEMO")
